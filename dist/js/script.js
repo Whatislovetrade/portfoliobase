583b62f747bbd001cd7283b5ffef249eafa662cb
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-12-16 16:19:02 +0500

    create blocks price and portfolio

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,35 @@
-'use strict'
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict'
 
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElement = document.querySelector('.menu__close'),
-      closeOverlay = document.querySelector('.menu__overlay')
+    const hamburger = document.querySelector('.hamburger'),
+          menu = document.querySelector('.menu'),
+          closeElement = document.querySelector('.menu__close'),
+          closeOverlay = document.querySelector('.menu__overlay')
+    
+    function menuToggle(selector, overlay, closeBtn) {
+        selector.addEventListener('click', () => {
+            menu.classList.toggle('active')
+        })
+    
+        overlay.addEventListener('click', () => {
+            menu.classList.remove('active')
+        })
+    
+        closeBtn.addEventListener('click', () => {
+            menu.classList.toggle('active')
+        })
+    
+    }
+    
+    menuToggle(hamburger, closeOverlay, closeElement)
 
-function menuToggle(selector, overlay, closeBtn) {
-    selector.addEventListener('click', () => {
-        menu.classList.toggle('active')
+
+    const persents = document.querySelectorAll('.persent'),
+          scales = document.querySelectorAll('.scale')
+    
+    persents.forEach((elem, i) => {
+        scales[i].style.width = elem.innerHTML
     })
+})
 
-    overlay.addEventListener('click', () => {
-        menu.classList.remove('active')
-    })
-
-    closeBtn.addEventListener('click', () => {
-        menu.classList.toggle('active')
-    })
-
-}
-
-menuToggle(hamburger, closeOverlay, closeElement)
 
